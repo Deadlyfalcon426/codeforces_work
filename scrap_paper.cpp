ef432320deed49cb235108513758e4f0fc93bff2
Ahsan <ahsan.moh426@gmail.com>
2026-03-26 19:43:17 -0400

    Initial commit

---
+++ scrap_paper.cpp
@@ -0,0 +1,17 @@
+/*
+Here, I'll be writing out pseudocode and stuff im testing, perhaps even notes.
+best_switch_number = vector.size()-1
+for x in vector.size()-1:
+    if vector[best_switch_number]<vector[x]:
+        best_switch_number=x
+
+flip flops":
+OtterZ has c combat power, n monsters to fight of their own monster power a, and k flip flops:
+He can kill a monster if combat power is >= monster power, which will give him all the monster power
+or he can throw flip flop and he will lose one but the monster combat power goes up by one
+MAX out his C
+
+
+
+*/
+
