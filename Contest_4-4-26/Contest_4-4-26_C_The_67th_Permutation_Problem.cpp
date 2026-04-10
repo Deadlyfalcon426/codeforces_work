f96f923d3c38abe33b3a22287f14ca2906888dbb
Ahsan <ahsan.moh426@gmail.com>
2026-04-10 11:26:10 -0400

    yeah so apparently i frogot .cpp at the end of a file somehow and then i tried to fix it by any means since it thought the issue was due to the new folder arangemenr. anyways i finished this problem so yeah. more on the problem in the actual code

--- Contest_4-4-26/Contest_4-4-26_C_The_67th_Permutation_Problem.cpp
+++ Contest_4-4-26/Contest_4-4-26_C_The_67th_Permutation_Problem.cpp
@@ -21,6 +21,7 @@ int main(){
     vector<int> current_sequence;
     vector<int> new_sequence;
     int counter = 0;
+    int counter_back = 0;
     for (int test_case = 0; test_case<testcases; test_case++){
         cin >> n;
         current_sequence.resize(3*n);
@@ -28,18 +29,18 @@ int main(){
         for (int i = 0; i<3*n; i++){
             current_sequence[i] = i+1;
         }
-        //switch up the array
+        //switch up the array, as well as print out
         counter=0;
+        counter_back = current_sequence.size()-1;
+        new_sequence.resize(3*n);
         for (int i = n; i>0; i--){
-            new_sequence[n-i] = current_sequence[i*3];
-            new_sequence[n-i+1] = current_sequence[(i*3)-1];
-            new_sequence[n-i+2] = current_sequence[counter];
+            cout << current_sequence[counter_back]<< " ";
+            counter_back--;
+            cout << current_sequence[counter_back]<< " ";
+            counter_back--;
+            cout << current_sequence[counter]<< " ";
             counter++;
         }
-        //print out ig
-        for (int item:new_sequence){
-            cout << item << " ";
-        }
-        cout<< "\n";
+        cout << "\n";
     }
 }
